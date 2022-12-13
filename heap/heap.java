09bb1b59fd5ac7631eacc8ca1e7be6705e85685a
Mohit kumar <64029983+mohitkumar000@users.noreply.github.com>
2022-12-14 03:30:06 +0530

    heap-complete

---
+++ heap/heap.java
@@ -0,0 +1,55 @@
+package heap;
+
+import java.util.*;
+
+public class heap {
+
+	
+	private ArrayList<Integer> list= new ArrayList<>();
+
+	public void add(int item)
+	{
+		list.add(item);
+		upheapify(list.size()-1);
+		
+	}
+
+	private void upheapify(int ci) {
+		// TODO Auto-generated method stub
+		int pi =(ci-1)/2;
+		if(list.get(pi)>list.get(ci));//child priority
+		{
+			swap(pi,ci);
+			upheapify(pi);
+			
+		}
+		
+		
+		
+		
+		
+	}
+	private void swap(int i,int j)
+	{
+		int ith = list.get(i);
+		int jth =list.get(j);
+		list.set(i, jth);
+		list.set(j, ith);
+		
+	}
+	
+	public int getmin()
+	{
+		return list.get(0);
+	}
+	public void display()
+	{
+		System.out.println(list);
+	}
+		
+
+	
+	
+	
+
+}
---
+++ heap/heap_client.java
@@ -0,0 +1,22 @@
+package heap;
+
+public class heap_client {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		heap hp = new heap();
+		
+		hp.add(2);
+		hp.add(3);
+		hp.add(5);
+		hp.add(1);
+		hp.add(7);
+		hp.add(-2);
+		hp.add(4);
+		hp.add(-7);
+		hp.add(11);
+		hp.display();
+
+	}
+
+}
