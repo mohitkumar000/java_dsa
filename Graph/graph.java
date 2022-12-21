427bab1742830491c40e0a1c388663651f945f76
Mohit kumar <64029983+mohitkumar000@users.noreply.github.com>
2022-12-21 18:04:20 +0530

    graph1

--- Graph/graph.java
+++ Graph/graph.java
@@ -1,10 +1,28 @@
 package Graph;
+import java.util.*;
 
 public class graph {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	HashMap<Integer ,HashMap<Integer,Integer>>map;
+	
+	public graph(int v)
+	{
+		map =new HashMap<>();
+		for(int i=1;i<=v;i++)
+		{
+			map.put(i,new HashMap<>());
+		}
 	}
+	public void AddEdge(int v1,int v2,int cost)
+	{
+		map.get(v1).put(v2,cost);
+		map.get(v2).put(v2,cost);
+		
+	}
+	public boolean ContainsEdge(int v1,int v2)
+	{
+		return map.get(v1).containsKey(v2);
+	}
+	
 
 }
