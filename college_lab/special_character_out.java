eff50f289b97c31ea4fcd67c75a941ab81eb3145
Mohit kumar <64029983+mohitkumar000@users.noreply.github.com>
2023-03-15 23:53:34 +0530

    commit

---
+++ college_lab/special_character_out.java
@@ -0,0 +1,17 @@
+package college_lab;
+
+public class special_character_out {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s = "112@45$34";
+		
+		
+
+	}
+	public static void odd_one_out(String s)
+	{
+		
+	}
+
+}
