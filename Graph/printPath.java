7d66f3a9dddfd3e0cca75e02f16bd0ba6f45a9fb
Mohit kumar <64029983+mohitkumar000@users.noreply.github.com>
2024-05-30 22:01:00 +0530

    commit

--- Graph/printPath.java
+++ Graph/printPath.java
@@ -23,3 +23,29 @@ public void printPath(int src,int des, HashSet<Integer>visited,string ans)
 
 }
 }
+package Graph;
+import java.util.*;
+public class printPath {
+public void printPath(int src,int des, HashSet<Integer>visited,string ans)
+{
+	
+	if(src==des)
+	{
+		System.out.println(ans);
+		return;
+		
+	}
+	visited.add(src);
+	for(int key:map.get(src).keySet())
+	{
+		if(!visited.contains(key))
+		{
+			boolean ans =printPath(key,des,visited,ans+src);
+			
+		}
+	}
+	visited.remove(src);
+
+}
+}
+
